6cd55d989f11a37c817d84805c47c8c66b207da7
ttoommoommii <69040360+ttoommoommii@users.noreply.github.com>
2023-11-23 06:46:37 +0900

    bak20211123

--- PoseNet/sketch.js
+++ PoseNet/sketch.js
@@ -41,20 +41,20 @@ let soundE;
 let img;
 let img2;
 let img3;
-let imgF=0;
+let imgF = 0;
 let img_LedRed;
-let img_LedX=10;
-let img_LedY=200;
+let img_LedX = 10;
+let img_LedY = 200;
 let img_On;
 let img_Off;
-let img_OnX=80;
-let img_OnY=100;
-let img_OffX=200;
-let img_OffY=80;
-let img_LedF=0;
+let img_OnX = 80;
+let img_OnY = 100;
+let img_OffX = 200;
+let img_OffY = 80;
+let img_LedF = 0;
 let img_LedWhite;
 
-let t=0.5;　//ポイントの移動割合0.25だと差の1/4移動
+let t = 0.5;　//ポイントの移動割合0.25だと差の1/4移動
 
 
 let previousRightHandY = 0;
@@ -64,7 +64,7 @@ let swf = false;
 let mon = false;
 
 let h;
-let www=640;
+let www = 640;
 
 let ww;
 let hh;
@@ -76,48 +76,48 @@ function preload() {
         img = loadImage("img/saru1.png");
         img2 = loadImage("img/banana.png");
         img3 = loadImage("img/apple.png");
-        img_LedRed=loadImage("img/led_red.png");
-        img_LedWhite=loadImage("img/led_white.png");
-        img_On=loadImage("img/button_off.png");
-        img_Off=loadImage("img/button_on.png")
+        img_LedRed = loadImage("img/led_red.png");
+        img_LedWhite = loadImage("img/led_white.png");
+        img_On = loadImage("img/button_off.png");
+        img_Off = loadImage("img/button_on.png")
 }
 //COM
 let port;
-  
-        async function onStartButtonClick() {
-          try {
-            port = await navigator.serial.requestPort();
-            await port.open({ baudRate: 115200 });
-            console.log("接続");
-          } catch (e) {
-            console.log("Error");
-          }
+
+async function onStartButtonClick() {
+        try {
+                port = await navigator.serial.requestPort();
+                await port.open({ baudRate: 115200 });
+                console.log("接続");
+        } catch (e) {
+                console.log("Error");
         }
-  
-        async function writeText(text) {
-          const encoder = new TextEncoder();
-          const writer = port.writable.getWriter();
-          await writer.write(encoder.encode(text));
-          console.log("テキスト書き込み: " + text);
-          writer.releaseLock();
-        }
+}
+
+async function writeText(text) {
+        const encoder = new TextEncoder();
+        const writer = port.writable.getWriter();
+        await writer.write(encoder.encode(text));
+        console.log("テキスト書き込み: " + text);
+        writer.releaseLock();
+}
 //-COM
 
 
 function windowResized() {
         //resizeCanvas(windowWidth,windowHeight-200*www);
-        resizeCanvas(video.width*www,video.height*www);
-        
+        resizeCanvas(video.width * www, video.height * www);
+
         //comButton.position(450, windowHeight-120);
         //OnButton.position(560, windowHeight-120);
         //OffButton.position(560, windowHeight-90);
-        comButton.position(video.width*www-250, video.height*www+90);
-        OnButton.position(video.width*www-150, video.height*www+90);
-        OffButton.position(video.width*www-150, video.height*www+120);
+        comButton.position(video.width * www - 250, video.height * www + 90);
+        OnButton.position(video.width * www - 150, video.height * www + 90);
+        OffButton.position(video.width * www - 150, video.height * www + 120);
 }
 // ページを開いたときに一度だけ実行する処理
 function setup() {
-        createCanvas(windowWidth-10,windowHeight-200);
+        createCanvas(windowWidth - 10, windowHeight - 200);
         video = createCapture(VIDEO);
         video.hide();
         let poseNet = ml5.poseNet(video, modelLoaded);
@@ -134,114 +134,119 @@ function setup() {
         posiP = createP('');    //デバックチェックを押した時に座標が表示される場所
 
         //
-        if(windowHeight > video.height) h=video.height;
-        else h=windowHeight;
-        if(windowWidth < windowHeight){
-            ww=windowWidth;hh=windowHeight;
-        }else{
-            ww=windowHeight;hh=windowWidth;
+        if (windowHeight > video.height) h = video.height;
+        else h = windowHeight;
+        if (windowWidth < windowHeight) {
+                ww = windowWidth; hh = windowHeight;
+        } else {
+                ww = windowHeight; hh = windowWidth;
         }
-        www=ww/video.width;
-        resizeCanvas(video.width*www,video.height*www+50*www);
+        www = ww / video.width;
+        resizeCanvas(video.width * www, video.height * www + 50 * www);
         //translate(width,0);
-        scale(-ww/(video.width-0),ww/(video.width-0));
+        scale(-ww / (video.width - 0), ww / (video.width - 0));
         //image(video, 0,0);
         //video.hide();
         //
         //com
         comButton = createButton("接続");
-        comButton.mousePressed(function() {
+        comButton.mousePressed(function () {
                 onStartButtonClick();
                 console.log("comButton");
         });
-        comButton.size(50,30);
-        comButton.position(450, windowHeight-120);
+        comButton.size(50, 30);
+        comButton.position(450, windowHeight - 120);
         //comButton.position(video.width*www-250, video.height+500);
-       
+
 
         OnButton = createButton("On");
-        OnButton.mousePressed(function() {
-                writeText('401\n');
+        OnButton.mousePressed(function () {
+                //writeText('401\r\n');
+                writeText('R\r\n');
                 console.log("On");
         });
-        OnButton.size(50,30);
-        OnButton.position(560, windowHeight-120);
+        OnButton.size(50, 30);
+        OnButton.position(560, windowHeight - 120);
         //OnButton.position(video.width*www-150, video.height+500);
-        
-        
+
+
         OffButton = createButton("Off");
-        OffButton.mousePressed(function() {
-                writeText('400\n');
+        OffButton.mousePressed(function () {
+                //writeText('400\r\n');
+                writeText('S000000000\r\n');
                 console.log("Off");
         });
-        OffButton.size(50,30);
-        OffButton.position(560, windowHeight-90);
+        OffButton.size(50, 30);
+        OffButton.position(560, windowHeight - 90);
         //OffButton.position(video.width*www-150, video.height+540);
         //com
 }
 
 // 定期的に繰り返し実行される処理
 function draw() {
-        if(windowHeight > video.height) h=video.height;
-        else h=windowHeight;
+        if (windowHeight > video.height) h = video.height;
+        else h = windowHeight;
         //if(windowWidth>video.width) www=video.width;
         //else www=windowWidth;
-        
-        if(windowWidth < windowHeight){
-            ww=windowWidth;hh=windowHeight;
-        }else{
-            ww=windowHeight;hh=windowWidth;
+
+        if (windowWidth < windowHeight) {
+                ww = windowWidth; hh = windowHeight;
+        } else {
+                ww = windowHeight; hh = windowWidth;
         }
 
-  
+
         let arm_ratio = 1.8;
         let leftHandX = leftElbowX + (leftWristX - leftElbowX) * arm_ratio;
         let leftHandY = leftElbowY + (leftWristY - leftElbowY) * arm_ratio;
 
         let rightHandX = rightElbowX + (rightWristX - rightElbowX) * arm_ratio;
         let rightHandY = rightElbowY + (rightWristY - rightElbowY) * arm_ratio;
-        
+
         //push(); //ここから反転
-        translate(width,0);
-        www=ww/video.width;
-        scale(-www,www);
-        image(video, 0,0);
+        translate(width, 0);
+        www = ww / video.width;
+        scale(-www, www);
+        image(video, 0, 0);
         //pop();
-        
-        if(swf){
+
+        if (swf) {
                 push();
-                translate(width,0);
+                translate(width, 0);
                 scale(-1.0, 1.0);   //左右反転
                 imageMode(CENTER);
-                image(img_Off,video.width*www-img_OnX,img_OnY,100,100);
-                image(img_On,video.width*www-img_OffX,img_OffY,100,100);
+                image(img_Off, video.width * www - img_OnX, img_OnY, 100, 100);
+                image(img_On, video.width * www - img_OffX, img_OffY, 100, 100);
                 pop();
-                if(img_LedF==0 && rightHandX<img_OnX+20 && rightHandX>img_OnX-20 && rightHandY<img_OnY){
-                        img_LedF=1;
+                if (img_LedF == 0 && rightHandX < img_OnX + 20 && rightHandX > img_OnX - 20 && rightHandY < img_OnY) {
+                        img_LedF = 1;
                         soundS.play();
-                        writeText('401\n');
+                        //writeText('401\r\n');
+                        writeText('R\r\n');
+
                 }
-                if(img_LedF==1 && leftHandX<img_OffX+20 && leftHandX>img_OffX-20 && leftHandY<img_OffY){
-                        img_LedF=0;
+                if (img_LedF == 1 && leftHandX < img_OffX + 20 && leftHandX > img_OffX - 20 && leftHandY < img_OffY) {
+                        img_LedF = 0;
                         soundE.play();
-                        writeText('400\n');
+                        //writeText('400\r\n');
+                        writeText('S000000000\r\n');
                 }
-                if(img_LedF==0){
-                        image(img_LedWhite,img_LedX,img_LedY,80,120);
-                }else{
-                        image(img_LedRed,img_LedX,img_LedY,80,120);
+                if (img_LedF == 0) {
+                        image(img_LedWhite, img_LedX, img_LedY, 80, 120);
+                } else {
+                        image(img_LedRed, img_LedX, img_LedY, 80, 120);
                 }
         }//sw
 
-        if(mon==true && imgF===1){
-                let w = dist(righteyeX,righteyeY,lefteyeX,lefteyeY);
+        if (mon == true && imgF === 1) {
+                let w = dist(righteyeX, righteyeY, lefteyeX, lefteyeY);
                 push();
                 imageMode(CENTER);
-                image(img , noseX , noseY-1.0*w/2 , w*5 , w*4.5);
-                image(img2 , rightHandX , rightHandY , w*3 , w*3);
-                image(img3 , leftHandX , leftHandY , w*3 , w*3);
+                image(img, noseX, noseY - 1.0 * w / 2, w * 5, w * 4.5);
+                image(img2, rightHandX, rightHandY, w * 3, w * 3);
+                image(img3, leftHandX, leftHandY, w * 3, w * 3);
                 pop();
-        //ellipse(noseX, noseY, 10, 10);
+                //ellipse(noseX, noseY, 10, 10);
         }
 
         // デバッグ用
@@ -260,10 +265,10 @@ function draw() {
                 ellipse(leftHandX, leftHandY, 10, 10);
                 ellipse(rightHandX, rightHandY, 10, 10);
                 posiP.html('　ハンドポジション LX:' + int(leftHandX) + ' LY:' + int(leftHandY) + ' RX:' + int(rightHandX) + ' RY:' + int(rightHandY));
-        }else{
+        } else {
                 posiP.html('');
         }
-        
+
         //scale(0.1,0.1);        
 }
 
@@ -276,7 +281,7 @@ function modelLoaded() {
 function gotPoses(results) {
         poses = results;
         if (poses.length > 0) {
-                imgF=1;
+                imgF = 1;
 
                 let newLeftShoulderX = poses[0].pose.leftShoulder.x;
                 let newLeftShoulderY = poses[0].pose.leftShoulder.y;
@@ -337,11 +342,11 @@ function gotPoses(results) {
                 noseY = lerp(noseY, newNoseY, t);
                 lefteyeX = lerp(lefteyeX, newLeftEyeX, t);
                 lefteyeY = lerp(lefteyeY, newLeftEyeY, t);
-                righteyeX = lerp(righteyeX, newRigthEyeX , t);
+                righteyeX = lerp(righteyeX, newRigthEyeX, t);
                 righteyeY = lerp(righteyeY, newRigthEyeY, t);
         }
-        else{
-                imgF=0;
+        else {
+                imgF = 0;
         }
 }
 
@@ -367,4 +372,3 @@ function toggleMon() {
                 mon = false;
         }
 }
-       
